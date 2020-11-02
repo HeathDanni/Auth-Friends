9c6e0bedba6918c49029e647fe535e7ed731a07d
Heather Beckman <heatherbeckman85@gmail.com>
2020-11-01 19:50:09 -0700

    added PrivateRoute an FriendsList components

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
     const [formInput, setFormInput] = useState({
         credentials: {username: '',
         password: ''}
